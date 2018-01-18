5aacb4b0d91afb993925dde3c4825ec5696d44da
Md. Moklesur Rahman <moklesur42@gmail.com>
2018-01-18 13:17:33 +0600

    Fix home url bug

--- sidebar.php
+++ sidebar.php
@@ -11,7 +11,6 @@ if ( ! is_active_sidebar( 'sidebar-1' ) ) {
 	return;
 }
 ?>
-
 <aside id="secondary" class="widget-area col-xl-3 col-lg-3 col-md-12 col-12 mt-50">
 	<?php
 	if ( class_exists( 'WooCommerce' ) && ( is_shop() || is_product() ) ) {
